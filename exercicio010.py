e6835dcd1de72c98e0b5ab5ed97f6fda228e67c0
Natalia Terra <dbnatty@gmail.com>
2021-02-24 16:56:36 -0300

    Update exercicio010.py

--- exercicio010.py
+++ exercicio010.py
@@ -1,5 +1,5 @@
 #Exercício Python 10: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. 
 
-n = float(input("Quanto você tem na carteira?"))
-dolar = n/5.42
-print("Com R${} você pode comprar ${}.".format(n, dolar))
+n = float(input("Quanto você tem na carteira? "))
+#dolar = n/5.42
+print("Com R${:.2f} você pode comprar ${:.2f}.".format(n, n/5.42))
