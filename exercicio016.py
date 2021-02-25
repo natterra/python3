3c4c44a35aec3489ba26c5b92a508fd642ebb6ad
Natalia Terra <dbnatty@gmail.com>
2021-02-25 09:35:21 -0300

    Create exercicio016.py

---
+++ exercicio016.py
@@ -0,0 +1,5 @@
+#Exercício Python 16: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+
+n = float(input("Digite um número: "))
+
+print(f"A parte inteira de {n} é {int(n)}.")
