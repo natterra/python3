6f2309eee259f460ad8867529483764356745c10
Natalia Terra <dbnatty@gmail.com>
2021-02-27 10:30:46 -0300

    Create exercicio025.py

---
+++ exercicio025.py
@@ -0,0 +1,5 @@
+# Exercício Python 25: Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.
+
+nome = input("Digite o nome completo: ")
+
+print("silva" in nome.lower()) 
