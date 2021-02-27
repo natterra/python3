06ae23f888b7574a0860efd2c939dae37263ea48
Natalia Terra <dbnatty@gmail.com>
2021-02-26 21:25:59 -0300

    Update exercicio024.py

--- exercicio024.py
+++ exercicio024.py
@@ -1,3 +1,6 @@
 # Exercício Python 24: Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome “SANTO”.
 
 cidade = input("Digite o nome da cidade: ")
+cid = cidade.strip()
+cid = cid[:5]
+print(cid.lower() == "santo")
